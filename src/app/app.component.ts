cb9211363dca80f1552c6026a442a63cf84149a0
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-10-05 20:07:53 +0700

    Fix bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,30 +19,30 @@ import { BustrackingPage } from '../pages/bustracking/bustracking';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage:any = OpeningPage;
-  
-  pages: Array<{title: string, component:any}>;
+  rootPage: any = OpeningPage;
+
+  pages: Array<{ title: string, component: any }>;
 
   constructor(
-    public platform: Platform, 
-    public statusBar: StatusBar, 
+    public platform: Platform,
+    public statusBar: StatusBar,
     public splashScreen: SplashScreen
-    ) {  
+  ) {
     this.initializeApp();
 
-      this.pages = [
-        {title: 'Home', component: HomePage},
-        {title: 'Guide Me', component: GuidemePage},
-        {title: 'Halte Location', component: HaltelocationPage},
-        {title: 'Bus Tracking', component: BustrackingPage},
-        {title: 'Parking History', component: ParkinghistoryPage},
-        {title: 'Account Settings', component: AccountsettingsPage},
-        {title: 'Logout', component: LogoutPage}
-      ];
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'Guide Me', component: GuidemePage },
+      { title: 'Halte Location', component: HaltelocationPage },
+      { title: 'Bus Tracking', component: BustrackingPage },
+      { title: 'Parking History', component: ParkinghistoryPage },
+      { title: 'Account Settings', component: AccountsettingsPage },
+      { title: 'Logout', component: LogoutPage }
+    ];
   }
-    
-  initializeApp(){
-      this.platform.ready().then(() => {
+
+  initializeApp() {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
